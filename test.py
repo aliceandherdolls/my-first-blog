ba443591eb566f8ff7b15007b37230852a7b3c6e
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2019-09-28 15:28:39 +0200

    obraz

---
+++ test.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3, 100, 4, 5, 6, 7, 9]
+
+
+def foo(argumentfunkcji):
+    # result = []
+    # opreacje na listach + petla for
+    # elementy > 4
+    # if ... else ...
+    for element in argumentfunkcji:
+        if element > 4:
+            print(element)
+
+
+
+    # print(result)
+
+
+foo(a)
